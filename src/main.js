4c7b3d06c4b5b9c4d18b8f7f48188e3aeb899ad0
MrWillCom <mr.will.com@outlook.com>
2022-04-06 07:49:59 +0800

    Add optional grey separator and `ls` command

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
+#!/usr/bin/env node
+
 import fs from 'node:fs'
 import child_process from 'node:child_process'
 import os from 'node:os'
+import minimist from 'minimist'
+import chalk from 'chalk'
 
-console.log(fs.readFileSync('./.lsnotes').toString())
+const argv = minimist(process.argv.slice(2))
 
-console.log('---\n')
+try {
+    console.log(fs.readFileSync('./.lsnotes').toString())
+} catch (err) {
+    if (err.code === 'ENOENT') {
+        console.log(chalk.grey('No .lsnotes.'))
+    }
+}
 
-const ls = os.platform() === 'win32' ? 'dir' : 'ls'
+if (argv.s === true || argv.separator === true) {
+    for (let i = 0; i < process.stdout.columns; i++) {
+        process.stdout.write(chalk.grey('-'))
+    }
+}
 
-console.log(child_process.execSync(ls).toString())
+if (argv.l === true || argv.ls === true) {
+    const ls = os.platform() === 'win32' ? 'dir' : 'ls'
+
+    console.log(child_process.execSync(ls).toString())
+}
