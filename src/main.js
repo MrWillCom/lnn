db716fb5fb0fcc2bc30f1a9ed75b619c9cb4a1fa
MrWillCom <mr.will.com@outlook.com>
2022-04-06 11:17:47 +0800

    Update read file encoding

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import chalk from 'chalk'
 const argv = minimist(process.argv.slice(2))
 
 try {
-    console.log(fs.readFileSync('./.lsnotes').toString())
+    console.log(fs.readFileSync('./.lsnotes', 'utf8'))
 } catch (err) {
     if (err.code === 'ENOENT') {
         console.log(chalk.grey('No .lsnotes.'))
@@ -18,6 +18,7 @@ if (argv.s === true || argv.separator === true) {
     for (let i = 0; i < process.stdout.columns; i++) {
         process.stdout.write(chalk.grey('-'))
     }
+    console.log('\n')
 }
 
 if (argv.l === true || argv.ls === true) {
